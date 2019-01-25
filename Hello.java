c74af2a929dcd75ef0ae7d784a2a92a4fa6484b9
kumaravelpre10 <kumaravelpre10@gmail.com>
2019-01-25 04:03:47 +0000

    Hello.java

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello{
 
          for(int i=1;i<=10;i++){
 
-              System.out.println("ITS NEW1ww webhook  KUMARAVEL FOR DEVOPS ENGINEER key DEPLOYED SUCCESFFULLY 15234.. this is my second project try for base level"+i);
+              System.out.println("ITS NEW1ww webhook  KUMARAVEL FOR DEVOPS ENGINEER trainee key DEPLOYED SUCCESFFULLY 15234.. this is my second project try for base level"+i);
       
          } 
  
