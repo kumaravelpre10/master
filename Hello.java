40852b65693f18057aad7f99dcf32c5b68a74e4e
kumaravelpre10 <45900646+kumaravelpre10@users.noreply.github.com>
2019-01-27 00:59:35 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello{
 
          for(int i=1;i<=10;i++){
 
-              System.out.println("ITS NEW1ww webhook  KUMARAVEL FOR DEVOPS ENGINEER trainee key DEPLOYED SUCCESFFULLY 15234.. this is my second project try for base level"+i);
+              System.out.println("ITS NEW GIT WEB HOOK  KUMARAVEL FOR DEVOPS ENGINEER trainee key DEPLOYED SUCCESFFULLY 15234.. this is my second project try for base level"+i);
       
          } 
  
